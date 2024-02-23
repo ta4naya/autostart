497f53751ed7def445d3c1b17cec45dec1b6a7ec
ta4naya <ta4naya@yahoo.com>
2024-02-23 02:14:40 +0100

    Update autostart.ps1

--- autostart.ps1
+++ autostart.ps1
@@ -1,18 +1,23 @@
-params (
-    [string[]]$resourcegroups
+param (
+    [string[]]$resourceGroups
 )
 
-function  AutoVMStart {
-    [string]$ResourceGroupName
+function AutoVMStart {
+    param (
+        [string]$ResourceGroupName
+    )
+
     $vms = Get-AzVm -ResourceGroupName $ResourceGroupName
-     # Start each VM
+    # Start each VM
     foreach ($vm in $vms) {
-        
+        Write-Host "Starting VM: $($vm.Name) in Resource Group: $ResourceGroupName"
         Start-AzVM -ResourceGroupName $ResourceGroupName -Name $vm.Name -ErrorAction Continue
     }
 }
 
-Connect-Az -Identity
-foreach ($rg in $resourcegroups) {
+# Assuming Managed Identity for Azure authentication
+Connect-AzAccount -Identity
+
+foreach ($rg in $resourceGroups) {
     AutoVMStart -ResourceGroupName $rg
 }
